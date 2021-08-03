d8b1fd87af6f76f5b301dd514019000f2f4619d9
Galax028 <p.suksiriwan@gmail.com>
2021-08-03 11:09:32 +0700

    Fix more things since Vercel is bruh

--- proxy.py
+++ proxy.py
@@ -1,11 +1,11 @@
 import requests
 from flask import Flask, send_from_directory
 
-server = Flask(__name__)
+app = Flask(__name__)
 
 
-@server.get("/")
-@server.get("/<string:file>")
+@app.get("/")
+@app.get("/<string:file>")
 def startpage(file = None):
     if not file:
         return send_from_directory("./", "startpage.html")
@@ -13,12 +13,12 @@ def startpage(file = None):
         return send_from_directory("./", file)
 
 
-@server.get("/ac/<string:query>")
+@app.get("/ac/<string:query>")
 def deal_with_cors(query):
     return {"data": requests.get(f"https://ac.duckduckgo.com/ac/?q={query}&type=list").json()[1]}
 
 
-@server.after_request
+@app.after_request
 def actually_deal_with_cors(res):
     res.headers["Access-Control-Allow-Origin"] = "startpage.galax.tech"
     return res
